0e3d3405990438faf9fea499557dd87531b0cd0d
Francesco C <fran461j@outlook.it>
2022-10-18 14:15:26 +0200

    primo script

---
+++ assets/script/main.js
@@ -0,0 +1,6 @@
+let nome = prompt('come ti chiami?');
+let cognome = prompt('e il cognome fra?');
+let colore = prompt('che colore ti piace?');
+document.getElementById('display').innerHTML = `non usare la password tipo :" ${nome}${cognome}${colore}21"`;
+
+console.log (nome);
